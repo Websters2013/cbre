b72b077b9a8b9d394d8b884d3cdcb6b8e86d27e1
iversontb <petruninevgeniy@gmial.com>
2017-04-07 05:18:23 +0300

    index

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -27,37 +27,21 @@
         //private properties
         var _obj = obj,
             _window = $( window ),
-            _scrollConteiner = $( 'html' ),
+            _scrollConteiner = $( '.site' ),
             _sceneItem = _obj.find( '.site__scene-item' ),
             _sceneFirstItem = _obj.find( '.site__first-scene' ),
             _sceneSecondItem = _obj.find( '.site__second-scene' ),
             _sceneSwipeItem = _obj.find( '.site__third-scene' ),
             _swipeItem = _sceneSwipeItem.find( '.services__item' ),
-            _site = $( '.site' ),
-            _header = _site.find( '.site__header' ),
+            _header = _scrollConteiner.find( '.site__header' ),
+            _heroSkip = _sceneFirstItem.find( '.hero__skip' ),
             _indicator,
             _indicatorSwiper,
-            _canScroll = false,
-            _sceneActive = true,
-            _menu = new Menu( $('.menu') );
+            _sceneActive = true;
 
         //private methods
         var _onEvent = function(){
                 _window.on( {
-                    'scroll': function () {
-
-                        if ( _window.scrollTop() == 0 && _sceneActive ){
-                            _obj.removeClass( 'hide' );
-                            _hidePageScroll();
-                        } else {
-                            setTimeout( function () {
-                                _sceneActive = true;
-                                _indicatorSwiper.turnOn();
-                                return false;
-                            }, 1000 );
-                        }
-
-                    },
                     'keydown': function ( e ) {
                         switch( e.which ) {
 
@@ -88,6 +72,28 @@
                         }
                     }
                 } );
+                _scrollConteiner.on( {
+                    'scroll': function () {
+
+                        if ( _scrollConteiner.scrollTop() == 0 && _sceneActive ){
+                            _obj.removeClass( 'hide' );
+                            _indicatorSwiper.turnOn();
+                            _hidePageScroll();
+                        } else {
+                            setTimeout( function () {
+                                _sceneActive = true;
+                                return false;
+                            }, 1000 );
+                        }
+
+                    }
+                } );
+                _heroSkip.on( {
+                    'click': function () {
+                        _checkScroll( 1 );
+                        return false;
+                    }
+                } );
                 _indicator = new WheelIndicator( {
                     elem: document.querySelector( '.site__section-scenes' ),
                     callback: function( e ){
@@ -102,7 +108,7 @@
             },
             _animateMainElement = function () {
 
-                _site.addClass( 'animate' );
+                _scrollConteiner.addClass( 'animate' );
 
             },
             _animateSwiper = function () {
@@ -129,25 +135,11 @@
                 } );
 
             },
-            _getScrollWidth = function (){
-                var scrollDiv = document.createElement( 'div'),
-                    scrollBarWidth;
-
-                scrollDiv.className = 'scroll__scrollbar-measure';
-
-                document.body.appendChild( scrollDiv );
-
-                scrollBarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-
-                document.body.removeChild(scrollDiv);
-
-                return scrollBarWidth;
-            },
             _checkScroll = function ( direction ) {
-                if ( direction > 0 && !_menu.opened ){
+                if ( direction > 0 ){
                     _checkSceneDown();
                 }
-                else if ( direction < 0 && _window.scrollTop() == 0 && !_menu.opened ) {
+                else if ( direction < 0 && _window.scrollTop() == 0 ) {
                     _checkSceneUp();
                 }
             },
@@ -155,7 +147,9 @@
 
                 var curElem = _sceneItem.filter( '.active' );
 
-                if ( curElem.index() >= 1 ) {
+                console.log( curElem.data( 'index' ) )
+
+                if ( curElem.data( 'index' ) - 1 >= 1 ) {
 
                     curElem.each( function () {
                         $( this ).prev().removeClass( 'prev' );
@@ -176,7 +170,7 @@
                 var curElem = _sceneItem.filter( '.active' ),
                     lengthItems = _sceneItem.length;
 
-                if ( curElem.index() + 1 < lengthItems ) {
+                if ( curElem.data( 'index' ) < lengthItems ) {
 
                     curElem.each( function () {
 
@@ -197,6 +191,8 @@
                     return false;
 
                 }
+
+                _showCircles();
 
             },
             _checkSwiperActive = function () {
@@ -226,10 +222,10 @@
 
             },
             _checkSwiperScroll = function ( direction ) {
-                if ( direction > 0 && !_menu.opened ){
+                if ( direction > 0 ){
                     _checkSwiperDown();
                 }
-                else if ( direction < 0 && !_menu.opened ) {
+                else if ( direction < 0 ) {
                     _checkSwiperUp();
                 }
             },
@@ -308,31 +304,36 @@
                 }
 
             },
+            _showCircles = function () {
+
+                _scrollConteiner.addClass( 'show-circles' )
+
+            },
             _hideScenes = function () {
                 _obj.addClass( 'hide' );
                 _indicator.turnOff();
                 _sceneActive = false;
                 _scrollConteiner.css( {
-                    overflowY: 'auto',
-                    paddingRight: 0
-                } );
-                _header.css( {
-                    paddingRight: 0
+                    overflowY: 'auto'
                 } );
             },
             _hidePageScroll= function () {
+
                 _scrollConteiner.css( {
-                    overflowY: 'hidden',
-                    paddingRight: _getScrollWidth()
+                    overflowY: 'hidden'
                 } );
-                _header.css( {
-                    paddingRight: _getScrollWidth()
-                } );
+
             },
             _construct = function() {
-                _animateMainElement();
-                _hidePageScroll();
-                _onEvent();
+
+                if ( _window.outerWidth() >= 1200 ) {
+
+                    _hidePageScroll();
+                    _animateMainElement();
+                    _onEvent();
+
+                }
+
             };
 
         //public properties
@@ -347,21 +348,27 @@
         //private properties
         var _obj = obj,
             _objTop = _obj.offset().top,
-            _window = $( window );
+            _window = $( '.site' );
 
         //private methods
         var _onEvent = function(){
                 _window.on( {
                     'scroll': function () {
 
-                        if ( ( _window.scrollTop() + _window.height() / 2 ) >= _objTop ){
-                            _obj.addClass( 'animate-block' )
-                        }
+                        _checkScroll();
 
                     }
                 } );
             },
+            _checkScroll = function () {
+
+                if ( ( _window.scrollTop() + _window.height() / 2 ) >= _objTop ){
+                    _obj.addClass( 'animate-block' )
+                }
+
+            },
             _construct = function() {
+                _checkScroll();
                 _onEvent();
             };
 
@@ -377,21 +384,27 @@
         //private properties
         var _obj = obj,
             _objTop = _obj.offset().top,
-            _window = $( window );
+            _window = $( '.site' );
 
         //private methods
         var _onEvent = function(){
                 _window.on( {
                     'scroll': function () {
 
-                        if ( ( _window.scrollTop() + _window.height() / 2 )  >= _objTop ){
-                            _obj.addClass( 'animate-block' )
-                        }
+                        _checkScroll();
 
                     }
                 } );
             },
+            _checkScroll = function () {
+
+                if ( ( _window.scrollTop() + _window.height() / 2 )  >= _objTop ){
+                    _obj.addClass( 'animate-block' )
+                }
+
+            },
             _construct = function() {
+                _checkScroll();
                 _onEvent();
             };
 
@@ -408,6 +421,7 @@
         var _obj = obj,
             _servicesSlider = _obj.find( '.services__swipe' ),
             _servicesPagination = _obj.find( '.services__pagination' ),
+            _servicesItem = _obj.find( '.services__item' ),
             _services;
 
         //private methods
@@ -420,7 +434,29 @@
                     slidesPerView: 1,
                     loop: false,
                     pagination: _servicesPagination,
-                    paginationClickable: true
+                    paginationClickable: true,
+                    onSlideChangeStart: function () {
+                        var curItem = _servicesItem.filter( '.swiper-slide-active' );
+
+                        curItem.addClass( 'animate-slide' );
+
+                        curItem.find( '.tlt' ).textillate( {
+                            in: {
+                                effect: 'fadeIn',
+                                delay: 3,
+                                shuffle: true
+                            }
+                        } );
+
+                        curItem.find( '.tlt-btn' ).textillate( {
+                            initialDelay: 200,
+                            in: {
+                                effect: 'fadeIn',
+                                delay: 50,
+                                shuffle: true
+                            }
+                        } );
+                    }
                 } );
 
             },
--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -1,51 +1,51 @@
-$( function(){
+( function(){
 
-    $.each( $( '.site__mobile' ), function() {
-        new Menu( $( this ) );
+    $( function(){
+
+        $.each( $( '.site__mobile' ), function() {
+            new Menu( $( this ) );
+        } );
+
     } );
 
-} );
+    var Menu = function( obj ){
 
-var Menu = function( obj ){
+        //private properties
+        var _obj = obj,
+            _btn = $( '.mobile-menu-btn' );
 
-    //private properties
-    var _obj = obj,
-        _self = this,
-        _btn = $( '.mobile-menu-btn' );
+        //private methods
+        var _constructor = function(){
+                _onEvents();
+            },
+            _onEvents = function(){
 
-    //private methods
-    var _constructor = function(){
-            _onEvents();
-        },
-        _onEvents = function(){
+                _btn.on( 'click', function() {
 
-            _btn.on( 'click', function() {
+                    if ( $( this ).hasClass( 'close' ) ){
+                        _closeMenu();
+                    } else {
+                        _openMenu();
+                    }
 
-                if ( $( this).hasClass( 'close' ) ){
-                    _closeMenu();
-                } else {
-                    _openMenu();
-                }
+                } );
 
-            } );
+            },
+            _openMenu = function(){
+                _btn.addClass( 'close' );
+                _obj.addClass( 'visible' );
+            },
+            _closeMenu = function(){
+                _btn.removeClass( 'close' );
+                _obj.removeClass( 'visible' );
+            };
 
-            _obj[ 0 ].obj = _self;
+        //public properties
 
-        },
-        _openMenu = function(){
-            _btn.addClass( 'close' );
-            _obj.addClass( 'visible' );
-        },
-        _closeMenu = function(){
-            _btn.removeClass( 'close' );
-            _obj.removeClass( 'visible' );
-        };
+        //public methods
 
-    //public properties
-    _self.opened = false;
+        _constructor();
 
-    //public methods
+    };
 
-    _constructor();
-
-};
+} )();
